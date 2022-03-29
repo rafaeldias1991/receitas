85ec684a047a9a6d4def109f04730d9478953092
rafael.dias <rafaelchavesdias8@gmail.com>
2022-03-29 10:43:56 -0300

    configuração heroku.

--- src/main/java/com/rafaelchavesdias/receitas/config/security/ConfiguracaoSeguranca.java
+++ src/main/java/com/rafaelchavesdias/receitas/config/security/ConfiguracaoSeguranca.java
@@ -25,7 +25,7 @@ public class ConfiguracaoSeguranca extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().anyRequest().authenticated().and().httpBasic().and()
+                .authorizeRequests().anyRequest().permitAll().and()
                 .csrf().disable();
 
     }
